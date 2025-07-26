ebda532245cc15bc206e7b45117d668c48a8a450
pozgayr <pozgay.richard@gmail.com>
2025-07-26 20:21:55 +0200

    commiting all changes that were made for testing purposes to clean main before creating new branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
+
 #include "board.hpp"
 
 int main() {
 	Board board;
 	
-	board.setBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+	board.setBoard("6k1/1pb3p1/2p4p/2Pp4/Pp1B2nq/3B3P/2Q1N1P1/5K2 b - - 0 1");
 	board.printBoard();
 	
 	return 0;
